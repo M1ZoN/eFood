35817e1ad5b66289b424988eeacc63d87749e236
Nozim <nazim.islamov99@gmail.com>
2019-04-04 18:25:48 -0700

    adding React workspace

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Our app is comming soon...</h1>
+        <h3>Stay tuned!</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
